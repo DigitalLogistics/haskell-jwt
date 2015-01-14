d604f583d4d10fe2112d3c86d964031e4269d427
Oliver Charles <ollie@ocharles.org.uk>
2015-01-14 16:06:44 +0000

    Update to haskell-ng

--- default.nix
+++ default.nix
@@ -1,26 +1,25 @@
-{ cabal, aeson, base64Bytestring, cryptohash, dataDefault
-, httpTypes, HUnit, network, QuickCheck, scientific, tasty
-, tastyHspec, tastyHunit, tastyQuickcheck, tastyTh, text, time
-, unorderedContainers, networkUri
-
-, doCheck ? false
+{ mkDerivation, aeson, base, base64-bytestring, bytestring
+, containers, cryptohash, data-default, http-types, HUnit, network
+, network-uri, QuickCheck, scientific, stdenv, tasty, tasty-hspec
+, tasty-hunit, tasty-quickcheck, tasty-th, text, time
+, unordered-containers
 }:
-
-cabal.mkDerivation (self: {
+mkDerivation {
   pname = "jwt";
   version = "0.2.1";
   src = ./.;
   buildDepends = [
-    aeson base64Bytestring cryptohash dataDefault httpTypes network
-    scientific text time unorderedContainers networkUri
+    aeson base base64-bytestring bytestring containers cryptohash
+    data-default http-types network network-uri scientific text time
+    unordered-containers
   ];
-  meta = {
-    homepage = "https://bitbucket.org/ssaasen/haskell-jwt";
-    description = "JSON Web Token (JWT) decoding and encoding";
-    license = self.stdenv.lib.licenses.mit;
-    platforms = self.ghc.meta.platforms;
-    noHaddock = true;
-    enableSplitObjs = false;
-  };
-  doCheck = false;
-})
+  testDepends = [
+    aeson base base64-bytestring bytestring containers cryptohash
+    data-default http-types HUnit network network-uri QuickCheck
+    scientific tasty tasty-hspec tasty-hunit tasty-quickcheck tasty-th
+    text time unordered-containers
+  ];
+  homepage = "https://bitbucket.org/ssaasen/haskell-jwt";
+  description = "JSON Web Token (JWT) decoding and encoding";
+  license = stdenv.lib.licenses.mit;
+}
